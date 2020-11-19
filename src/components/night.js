85a122fc8723d11015988d08cf2f1c126527aeca
YeChan Kang <kyechan99@gmail.com>
2020-11-19 10:33:29 +0000

    FIX : Nightmode localStorage bug on deploy

--- src/components/night.js
+++ src/components/night.js
@@ -3,45 +3,40 @@ import React from 'react';
 
 
 class NightCheckbox extends React.Component {
-     constructor(props) {
-       super(props);
-		 
-       this.tick = this.tick.bind(this);
+	constructor(props) {
+		super(props);
 
+		this.tick = this.tick.bind(this);
 
-       this.state = {
-         checkBox: false,
-         checkedMsg: 'LIGHT-MODE'
-       }
-	
-		 if (localStorage.toggled === 'dark') {
-			 console.log("IM IN! DARK");
-			document.documentElement.setAttribute('data-theme', 'dark');
-			 this.state.checkBox = true;
-			 this.state.checkedMsg = 'DARK-MODE';
+		this.state = {
+			checkBox: false,
+			checkedMsg: 'LIGHT-MODE'
 		}
-		 
-		 console.log(this.state.checkBox);
+	
+		if (typeof window !== 'undefined')
+			if (localStorage.toggled === 'dark') {
+				document.documentElement.setAttribute('data-theme', 'dark');
+				this.state.checkBox = true;
+				this.state.checkedMsg = 'DARK-MODE';
+			}
      }
 
-     tick() {
-		 if (this.state.checkBox) {
-			localStorage.toggled = '';
+	tick() {
+		if (this.state.checkBox) {
+			if (typeof window !== 'undefined')
+				localStorage.toggled = '';
 			document.documentElement.setAttribute('data-theme', 'light');
-		 } else {
-			localStorage.toggled = 'dark';
+		} else {
+			if (typeof window !== 'undefined')
+				localStorage.toggled = 'dark';
 			document.documentElement.setAttribute('data-theme', 'dark');
-		 }
-		 console.log('CLICK ME');
+		}
 		 
-		 
-       this.setState({
-         checkBox: !this.state.checkBox
-       })
-		 console.log('CLICK ME ', this.state.checkBox);
-
-       this.state.checkBox ? this.setState({ checkedMsg: 'DARK-MODE' }) : this.setState({ checkedMsg: 'LIGHT-MODE' })
-		 
+		this.setState({
+			checkBox: !this.state.checkBox
+		})
+		this.state.checkBox ? 
+			this.setState({ checkedMsg: 'DARK-MODE' }) : this.setState({ checkedMsg: 'LIGHT-MODE' })
      }
 
      render() {
